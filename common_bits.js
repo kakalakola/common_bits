7bc81d4ef096a8d5a26d97c664fa976f1df81371
Saad A <saad_azim@hotmail.com>
2017-12-14 16:07:17 +0600

    Update common_bits.js

--- common_bits.js
+++ common_bits.js
@@ -1,7 +1,7 @@
 /*
   COMMON_BITS
   By: Saad Azim
-  Last Edit: 2017.05.09
+  Last Edit: 2017.12.14
 
   I made this basically to make coding a little easier for me. The initial ideas behind this were:
     a) using easy to understand names
@@ -11,38 +11,69 @@
   Released under GNU GPL 3.0
   ... er ... that's about it.
 */
-var functionGetById
-    ,functionGetByTag
-    ,functionCreateElement
-    ,functionAppendScript
-    ,doc=document
 
-    ;
-functionGetById=function(strId){
-  "use strict";
-  return doc.getElementById(strId);
-};
-functionGetByTag=function(strTag){
-  "use strict";
-  return doc.getElementsByTagName(strTag);
-};
+/*jslint es6:true,this:true*/
+const doc=document
+      //Standard bits, wrapped in functions
+      ,functionGetById=function(strId){
+        "use strict";
+        return doc.getElementById(strId);
+      }
+      ,functionGetByTag=function(strTag){
+        "use strict";
+        return doc.getElementsByTagName(strTag);
+      }
+      ,functionGetByClass=function(strClass){
+        "use strict";
+        return doc.getElementsByClassName(strClass);
+      }
 
-functionCreateElement=function(strElement,strId){
-  "use strict";
-  var tmpElement=doc.createElement(strElement)
+      ,functionCreateElement=function(strElement,strId){
+        "use strict";
+        var tmpElement=doc.createElement(strElement)
+            ;
+        if(strId){
+          tmpElement.id=strId;
+        }
+        return tmpElement;
+      }
+      ,functionAppendScript=function(strSrc,domParent,functionOnLoad){
+        "use strict";
+        var domScript=functionCreateElement("script")
+            ;
+        domScript.type="text/javascript";
+        domScript.src=strSrc;
+        domScript.addEventListener("load",functionOnLoad,false);
+        domParent.appendChild(domScript);
+      }
+
+      //XHR request, now wrapped in promises
+      ,functionXHR=function(strUrl,formData,functionResponse){
+        "use strict";
+        //Make a promise object, with resolve(success) & reject(error)
+        return new Promise(function(resolve,reject){
+          //Actual XHR bits
+          const xhr=new XMLHttpRequest();
+          xhr.open("POST",strUrl,true);
+          //Standard load & error bits, but instead of functions as response, resolve & reject are set
+          xhr.onload=function(){
+            console.log("XHR load complete");
+            if(this.status>=200 && this.status<300){
+              resolve(xhr.response);
+            }else{
+              reject('{"status":"'+this.status+'","statusText":"'+xhr.statusText+'"}');
+            }
+          };
+          xhr.onerror=function(){
+            reject('{"status":"'+this.status+'","statusText":"'+xhr.statusText+'"}');
+          };
+          xhr.send(formData);
+        //The responses are called via .then() and .catch()
+        }).then(function(strResponse){
+          functionResponse(strResponse);
+        })
+        .catch(function(strResponse){
+          functionResponse(strResponse);
+        });
+      }
       ;
-  if(strId){
-    tmpElement.id=strId;
-  }
-  return tmpElement;
-};
-
-functionAppendScript=function(strSrc,domParent,functionOnLoad){
-  "use strict";
-  var domScript=functionCreateElement("script")
-      ;
-  domScript.type="text/javascript";
-  domScript.src=strSrc;
-  domScript.addEventListener("load",functionOnLoad,false);
-  domParent.appendChild(domScript);
-};
